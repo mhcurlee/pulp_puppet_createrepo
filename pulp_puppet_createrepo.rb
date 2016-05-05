d3c4445c7cb29b43e6d1a8979663cf6a0975a38e
root <root@katello.curlee.local>
2016-05-05 17:07:57 -0400

    refactored

--- pulp_puppet_createrepo.rb
+++ pulp_puppet_createrepo.rb
@@ -17,7 +17,7 @@ file = File.open(mypwd + "/PULP_MANIFEST", "w")
 files.each do |file_name|
   if !File.directory?(file_name) && file_name != "PULP_MANIFEST" 
 
-   file.write( file_name + ',' + Digest::SHA2.hexdigest(File.read(file_name)).to_s + ',' + File.size(file_name).to_s + "\n")
+   file.write("#{file_name},#{Digest::SHA2.hexdigest(File.read(file_name))},#{File.size(file_name)}\n")
   
   end
 end
